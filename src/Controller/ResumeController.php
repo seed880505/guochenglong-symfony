1f7b7e05d48b220387252c6879e1d2e3d274f556
Chenglong GUO <guochenglong0505@gmail.com>
2018-08-24 00:34:29 +0800

    fix swift mailer

--- src/Controller/ResumeController.php
+++ src/Controller/ResumeController.php
@@ -31,33 +31,29 @@ class ResumeController extends Controller
      */
     public function contactEmailAction(Request $request, \Swift_Mailer $mailer)
     {
-        $emailFrom = $request->request->get('c_y_email');
+        $email = $request->request->get('c_y_email');
         $subject = $request->request->get('c_y_subject');
         $message = $request->request->get('c_y_message');
         $cc = $request->request->get('c_y_cc');
 
         // empty
-        if (empty($emailFrom) || empty($subject) || empty($message)) {
+        if (empty($email) || empty($subject) || empty($message)) {
             throw new \Exception('Content could not be empty.', 500);
         }
 
         // email
-        if (filter_var($emailFrom, FILTER_VALIDATE_EMAIL) === false) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
             throw new \Exception('E-mail is not valid.', 500);
         }
 
         // send
-        $emailsTo = [$this->getParameter('mailer_to')];
-
-        if (!empty($cc)) {
-            $emailsTo[] = $emailFrom;
-        }
-
         $message = (new \Swift_Message($subject))
-            ->setFrom($emailFrom)
-            ->setTo($emailsTo)
+            ->setFrom($email)
+            ->setTo($this->getParameter('mailer_to'))
             ->setBody($message, 'text/plain');
-
+        if (!empty($cc)) {
+            $message->setCc($email);
+        }
         // result
         $sent_result = $mailer->send($message) > 0 ? 1 : 0;
 
